86d31dc3eea7ec3ba5d3ee6343f9c82c5ec7e1a4
Aziz Patel <ac98844@avcol.school.nz>
2021-10-01 15:32:04 +1300

    Fixed price in DB (edited the class then add new migration)

---
+++ AvondaleTyresOnlineShop/Migrations/20211001022340_fixpriceonDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AvondaleTyresOnlineShop.Migrations
+{
+    public partial class fixpriceonDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
